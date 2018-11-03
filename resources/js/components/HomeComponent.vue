1a7ea8d9f973c3cacdd01328f66760e118f2ac0b
Wallas Faria <wallasfaria@hotmail.com>
2018-11-03 10:53:03 -0300

    add home page

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,14 +1,12 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+    <div class="main">
+        <div class="logo">
+            <img src="svg/logo.svg" alt="Logo Opt">
+        </div>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
+        <div class="content">
+            <div class="search">
+                <input type="text">
             </div>
         </div>
     </div>
