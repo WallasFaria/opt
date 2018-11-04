1711dd53e3ebaac475bbfe0a2247eb7361334ab2
Wallas Faria <wallasfaria@hotmail.com>
2018-11-04 09:54:32 -0200

    Ajust details css

--- public/js/app.js
+++ public/js/app.js
@@ -71142,7 +71142,7 @@ var content = __webpack_require__(191);
 if(typeof content === 'string') content = [[module.i, content, '']];
 if(content.locals) module.exports = content.locals;
 // add the styles to the DOM
-var update = __webpack_require__(193)("9aafd618", content, false, {});
+var update = __webpack_require__(193)("b4a50b7e", content, false, {});
 // Hot Module Replacement
 if(false) {
  // When the styles change, update the <style> tags
